fb69969e07721b28ad7442364645d398be8499b9
AbdelghaniJamal <90258194+AbdelghaniJamal@users.noreply.github.com>
2021-12-01 16:17:04 +0300

    Add files via upload

---
+++ compile-openssl-1.1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Compile OpenSSL
+OPENSSL=openssl-1.1.1l.tar.gz
+
+DONE=openssl-compile-done
+
+if [ ! -f "${DONE}" ] ;then
+    wget https://www.openssl.org/source/${OPENSSL}
+
+    tar zxvf ${OPENSSL}
+
+    cd $(basename $OPENSSL .tar.gz)
+
+    ./config shared no-idea no-md2 no-mdc2 no-rc5 no-rc4 --prefix=/usr/local/
+
+    make
+
+    sudo make install
+
+    cd ..
+
+    touch ${DONE}
+fi
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/
+
+read -n1 -r -p "$(/usr/local/bin/openssl version) - Press any key to continue..." key
+
+source ./nginx-with-tls13-compile.sh
+
+## copy systemctl config
+cp nginx.service /lib/systemd/system/nginx.service
